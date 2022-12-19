99ef73c2069e2c044c69927fa0e00e689b542555
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-12-19 12:31:58 +0900

    fix: change params for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,26 @@
 # frozen_string_literal: true
+require 'debug'
 
 class UsersController < ApplicationController
   def index
     @users = User.order(:id).all.page params[:page]
   end
   def show
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
+    binding.break
   end
   def edit
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
+    binding.break
   end
   def update
-    user = User.find(current_user.id)
-    user.save
+    user = User.find(params[:id])
+    user.save!
     redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :postal_code, :address, :introduction)
   end
 end
