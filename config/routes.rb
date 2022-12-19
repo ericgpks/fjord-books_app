7c9e0fd241b8deac02b30f56aef506b35c0cb02f
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-12-19 16:58:27 +0900

    feat: add mechanism to send email to reset password

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show edit update]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
