69c53544a6864511bf00b5864dc63745a738479e
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-12-19 12:30:40 +0900

    feat: change db add columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
-  attr_accessor :email, :postal_code, :address, :introduction
+  attr_accessor :postal_code, :address, :introduction
 
   paginates_per 5
 end
