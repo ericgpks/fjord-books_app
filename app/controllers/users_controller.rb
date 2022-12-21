6afdad27773d1ac2f7bad860a13858ab199fe539
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-12-21 23:29:29 +0900

    feat: add follow method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'debug'
+
 class UsersController < ApplicationController
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
@@ -7,5 +9,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def follow
+    follow_user_id = params[:user_id].to_i
+    follow = Follow.new(
+      follow_user_id: follow_user_id,
+      follower_user_id: current_user.id
+    )
+    if follow.save
+      redirect_to users_path
+    end
   end
 end
