93e0ba1aa4d32b06f770c6c466cc86b399bc347c
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-12-23 22:49:07 +0900

    fix: change schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_21_115118) do
+ActiveRecord::Schema.define(version: 2022_12_23_123926) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_12_21_115118) do
     t.bigint "follower_user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["follow_user_id", "follower_user_id"], name: "index_follows_on_follow_user_id_and_follower_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
