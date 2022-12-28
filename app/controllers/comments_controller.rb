5bcd0f5ca103097c4701c08441ccef591b4cd96d
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-12-28 22:28:54 +0900

    fix: make format

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,8 @@
 
 class CommentsController < ApplicationController
   def create
-    permitted_params = comment_params
-    if params[:model] == "book_comment"
+    comment_params
+    if params[:model] == 'book_comment'
       book_comment = BookComment.new(
         user_id: current_user.id,
         book_id: params[:data].to_i,
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
       book_comment.save
       redirect_to book_path(params[:data].to_i)
     end
-    if params[:model] == "report_comment"
+    if params[:model] == 'report_comment'
       report_comment = ReportComment.new(
         user_id: current_user.id,
         report_id: params[:data].to_i,
@@ -24,6 +24,7 @@ class CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
     params.permit(:content, :model, :data)
   end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[ show edit update destroy ]
+  before_action :set_report, only: %i[show edit update destroy]
 
   # GET /reports or /reports.json
   def index
@@ -17,15 +19,14 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /reports or /reports.json
   def create
     @report = Report.new(report_params)
     @report.user_id = current_user.id
     if @report.save
-      redirect_to report_url(@report), notice: "Report was successfully created."
+      redirect_to report_url(@report), notice: 'Report was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -34,7 +35,7 @@ class ReportsController < ApplicationController
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
     if @report.update(report_params)
-      redirect_to report_url(@report), notice: "Report was successfully updated."
+      redirect_to report_url(@report), notice: 'Report was successfully updated.'
 
     else
       render :edit, status: :unprocessable_entity
@@ -44,17 +45,18 @@ class ReportsController < ApplicationController
   # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
-    redirect_to reports_url, notice: "Report was successfully destroyed."
+    redirect_to reports_url, notice: 'Report was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def report_params
-      params.require(:report).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def report_params
+    params.require(:report).permit(:title, :body)
+  end
 end
