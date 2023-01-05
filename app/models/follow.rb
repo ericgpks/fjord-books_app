adfba6b99714cc39abab91cb21a9cebffa5904da
ericgpks <sq.wacbc5.nyc@gmail.com>
2023-01-05 10:36:42 +0900

    fix: delete presence validation

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,6 +3,5 @@
 class Follow < ApplicationRecord
   belongs_to :follower_user, class_name: 'User'
   belongs_to :follow_user, class_name: 'User'
-  validates :follow_user_id, :follower_user_id, presence: true
   validates :follow_user_id, uniqueness: { scope: :follower_user_id }
 end
